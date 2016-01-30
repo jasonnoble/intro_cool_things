9221c5b052908d31355e22a108e159013cbd2a31
Jason Noble <github+jasonn@jasonnoble.org>
2016-01-30 14:08:42 -0700

    Added Email to Students

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130203455) do
+ActiveRecord::Schema.define(version: 20160130210810) do
 
   create_table "interesting_things", force: true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160130203455) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "omniauth",   default: true
+    t.string   "email"
   end
 
 end
